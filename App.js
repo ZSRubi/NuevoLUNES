669fba5eb592974acbf0669a159643bacb27688a
Rubí <ramseymegan17@gmail.com>
2025-04-08 11:13:58 -0500

    1

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
-const showMessage=(message)=>{
-    return message;
+//const showMessage=(message)=>{
+    //return message;
+//}
+//console.log(showMessage('hola coo etas'))
+//console.log("segundo ")
+var a=8
+if(a%2==0){
+    console.log("es un numero par");
+}else{
+    console.log("es un numero impar");
 }
-console.log(showMessage('hola coo etas'))
-console.log("segundo ")
